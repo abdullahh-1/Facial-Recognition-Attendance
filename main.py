40a73436f9a2f36b5e79bfc4b5580ffb5625e211
Chaudhary <34235002+Zakauddin@users.noreply.github.com>
2022-05-27 03:02:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,15 @@ if __name__ == '__main__':
     # if students are found
     if student_names:
         # reading previous data in csv
-        curr_date = datetime.date.today()
+        curr_date = str(datetime.date.today())
         f = open('attendance.csv', 'r')
-        data = csv.reader(f)
+        reader = csv.reader(f)
+        data = []
+        for row in reader:
+            data.extend(row)
+        f.close()
+
+        f = open('attendance.csv', 'r')
         last_line = f.readlines()[-1]
         last_name, comma, last_date = last_line.partition(',')
         if last_date == '':
@@ -20,19 +26,21 @@ if __name__ == '__main__':
             last_name = ''
         f.close()
 
+        last_date = last_date[:10]
+        curr_date = curr_date[:10]
+
         f = open('attendance.csv', 'a')
         writer_object = csv.writer(f)
 
         for name in student_names:
             if last_date == curr_date:
-                if functions.search_name(name, data):  # attendance is already marked
-                    exit(0)
-                else:
+                if not functions.search_name(name, data):  # attendance is already marked
                     new_entry = [name, curr_date]
                     writer_object.writerow(new_entry)
             else:
                 # new date
-                writer_object.writerow(curr_date)
+                c_date = [curr_date]
+                writer_object.writerow(c_date)
                 new_entry = [name, curr_date]
                 writer_object.writerow(new_entry)
 
